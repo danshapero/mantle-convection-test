9ba87415b941ad0a13ea0e5f752e330ef521d1f7
Daniel Shapero <shapero.daniel@gmail.com>
2025-06-17 12:32:11 -0700

    Add quiver plot of velocity

--- make-movie.py
+++ make-movie.py
@@ -19,22 +19,39 @@ with firedrake.CheckpointFile(args.input_filename, "r") as input_file:
     mesh = input_file.load_mesh()
     num_steps = input_file.h5pyfile.attrs["num_steps"]
 
-    Ts = []
+    us, Ts = [], []
     for step in range(num_steps + 1):
+        us.append(input_file.load_function(mesh, name="velocity", idx=step))
         Ts.append(input_file.load_function(mesh, name="temperature", idx=step))
 
-fig, ax = plt.subplots()
-ax.set_aspect("equal")
-ax.get_xaxis().set_visible(False)
-ax.get_yaxis().set_visible(False)
+fig, axes = plt.subplots(nrows=2, ncols=1, sharex=True, sharey=True)
+for ax in axes:
+    ax.set_aspect("equal")
+    ax.set_axis_off()
 
 fn_plotter = firedrake.FunctionPlotter(mesh, num_sample_points=4)
 kw = {"vmin": 0, "vmax": 1, "cmap": "inferno"}
-colors = firedrake.tripcolor(Ts[0], axes=ax, num_sample_points=4, **kw)
+colors = firedrake.tripcolor(Ts[0], axes=axes[0], num_sample_points=4, **kw)
 
-def animate(T):
+X = mesh.coordinates.dat.data_ro
+V = mesh.coordinates.function_space()
+v = us[0].copy(deepcopy=True)
+interpolator = firedrake.Interpolate(v, V)
+u_X = firedrake.assemble(interpolator)
+u_vals = u_X.dat.data_ro
+arrows = firedrake.quiver(us[0], axes=axes[1], cmap="inferno")
+
+def animate(fields):
+    u, T = fields
+
     colors.set_array(fn_plotter(T))
 
-iterator = Ts if not has_tqdm else tqdm.tqdm(Ts)
+    v.assign(u)
+    u_X.assign(firedrake.assemble(interpolator))
+    u_vals = u_X.dat.data_ro
+    arrows.set_UVC(*(u_vals.T))
+
+fields = list(zip(us, Ts))
+iterator = fields if not has_tqdm else tqdm.tqdm(fields)
 animation = FuncAnimation(fig, animate, iterator, interval=1e3 / args.framerate)
 animation.save(args.output_filename)
